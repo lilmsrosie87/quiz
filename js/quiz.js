5ae54ca01b8b28ce7be29a49454cfaf159a4cfda
Roslyn Harris <rharris0113@gmail.com>
2018-02-13 21:01:07 -0800

    working on array of questions for quiz

---
+++ js/quiz.js
@@ -0,0 +1,24 @@
+var questions = [
+{
+	question: "What district did Katnis Everden represent at the 74th Hunger Games?",
+	answer: [" District 12 ", " District 8 ", " District 2", " The Capital "],
+	correct: "District 12"
+},
+{
+	question: "What type of shop did Peeta Mellark's family own?",
+	answer:[" apothecary ", " auto ", " bakery ", " farm "],
+	correct: "bakery"
+},
+{
+	question: "In her first interview with Ceaser Flickerman, what does Katniss say is her favorite thing about the Capital?",
+	answer:[" the architecture ", " lamb stew with dried plums ", " everyone is so friendly ", " she doesn't say anything "],
+	correct: "lamb stew with dried plums"
+},
+{
+	question: "Who is the mentor for District 12 in the 74th Hunger Games?",
+	answer:[" Effie Trinket ", " Gayle Thompson ", " President Snow ", " Haymitch Abernathy "],
+	correct: "Haymitch Abernathy"
+}
+]
+
+console.log[2]
