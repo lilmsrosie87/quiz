a0a378fd1c9ddbfd47e95972525964beee16910e
Roslyn Harris <rharris0113@gmail.com>
2018-03-14 20:12:10 -0700

    added 'questionnaire' variable in js and aligned array of questions

--- js/quiz.js
+++ js/quiz.js
@@ -1,39 +1,43 @@
 // questions for quiz (week 10)
 var questions = [
-{
-	question: "What district did Katnis Everden represent at the 74th Hunger Games?",
-	answer: [" District 12 ", " District 8 ", " District 2", " The Capital "],
-	correct: "District 12"
-},
-{
-	question: "What type of shop did Peeta Mellark's family own?",
-	answer:[" apothecary ", " auto ", " bakery ", " farm "],
-	correct: "bakery"
-},
-{
-	question: "In her first interview with Ceaser Flickerman, what does Katniss say is her favorite thing about the Capital?",
-	answer:[" the architecture ", " lamb stew with dried plums ", " everyone is so friendly ", " she doesn't say anything "],
-	correct: "lamb stew with dried plums"
-},
-{
-	question: "Who is the mentor for District 12 in the 74th Hunger Games?",
-	answer:[" Effie Trinket ", " Gayle Thompson ", " President Snow ", " Haymitch Abernathy "],
-	correct: "Haymitch Abernathy"
-},
-{
-	question: "What three districts were known as 'the careers'?",
-	answer: [" District 13 ", "District 3 ", " The Capital ", " District 6 "],
-	correct: "District 3"
-},
-{
-	question: "What did Peeta through towards Katniss on that rainy day she saw the dandilions?",
-	answer: [" bread ", " basketball ", " advice ", " a coin from the Capital "],
-	correct: "bread"
-},
-	question: "Who is the only person Katnis is certain that she loves?",
-	answer: [" Peeta ", " Prim ", " Gayle ", " her mother "],
-	correct: "Prim"
-}
-]
+	{
+		question: "What district did Katnis Everden represent at the 74th Hunger Games?",
+		answer: [" District 12 ", " District 8 ", " District 2", " The Capital "],
+		correct: "District 12"
+	},
+	{
+		question: "What type of shop did Peeta Mellark's family own?",
+		answer:[" apothecary ", " auto ", " bakery ", " farm "],
+		correct: "bakery"
+	},
+	{
+		question: "In her first interview with Ceaser Flickerman, what does Katniss say is her favorite thing about the Capital?",
+		answer:[" the architecture ", " lamb stew with dried plums ", " everyone is so friendly ", " she doesn't say anything "],
+		correct: "lamb stew with dried plums"
+	},
+	{
+		question: "Who is the mentor for District 12 in the 74th Hunger Games?",
+		answer:[" Effie Trinket ", " Gayle Thompson ", " President Snow ", " Haymitch Abernathy "],
+		correct: "Haymitch Abernathy"
+	},
+	{
+		question: "What three districts were known as 'the careers'?",
+		answer: [" District 13 ", "District 3 ", " The Capital ", " District 6 "],
+		correct: "District 3"
+	},
+	{
+		question: "What did Peeta through towards Katniss on that rainy day she saw the dandilions?",
+		answer: [" bread ", " basketball ", " advice ", " a coin from the Capital "],
+		correct: "bread"
+	},
+		question: "Who is the only person Katnis is certain that she loves?",
+		answer: [" Peeta ", " Prim ", " Gayle ", " her mother "],
+		correct: "Prim"
+	}
+];
 
-console.log[2]
+
+// this is the attribute used to start quiz (activates onlick() in questionnaire)
+var questionnaire = getElementById('questionnaire');
+
+function startGame()
