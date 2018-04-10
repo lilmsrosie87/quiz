139e951332b82d8bcd09a42794e0ef7983a43042
Roslyn Harris <rharris0113@gmail.com>
2018-04-09 21:15:33 -0700

    added attributes to node created

--- js/quiz.js
+++ js/quiz.js
@@ -37,54 +37,69 @@ var questions = [
 ];
 
 
-// this is the attribute used to start quiz (activates onlick() in questionnaire)
-var questionnaire = getElementById("questionnaire");
+	// this is the attribute used to start quiz (activates onlick() in questionnaire)
+	var questionnaire = getElementById("questionnaire");
 
-function startGame() {
-	// clears previous HTML
-	questionnaire.innerHTML = " ";
-	
-	// centers questions in quiz
-	questionnaire.style.margin = "12% auto";
-	
-	// load 1st question
-	createQuestion();
+	function startGame() {
+		// clears previous HTML
+		questionnaire.innerHTML = " ";
+		
+		// centers questions in quiz
+		questionnaire.style.margin = "12% auto";
+		
+		// load 1st question
+		createQuestion();
+	};
+		
+	var createQuestion() {
+		// clears previous HTML
+		questionnaire.innerHTML = "";
+		
+	// loop
+	for (var i = 0; i < i++) {
+		
+		// elements that will be created when page loads
+		var formGroup = document.createElement("div");
+		var questionEl = document.createElement("h2");
+		
+		// attributes that will be added to elements above
+		formGroup.className = "formGroup";
+		questionEl.id = "questions" + [i];
+		
+		// will create text for question
+		var questionText = document.createTextNode(questions[i].question);
+		
+		// will add question to element
+		questionEl.appendChild(questionText);
+		
+		// will add element to DOM
+		formGroup.appendChild(questionEl);
+		
+		// will add formGroup to questionForm
+		questionForm.appendChild(formGroup);
+		
+		// adding questions to be displayed in DOM
+	for ( var r = 0; r <questions[i].answers.length; r++) {
+		var answerDiv = document.createElement("div");
+		var answerEl = document.createElement("input");
+		
+		//node for text question
+	var answerText = document.createTextNode(questions[i].answers[r];
+
+	answerDiv. appendChild(answerEl);
+	answerDiv.appendChild(answerChild(answerText);
+
+	// adding attributes
+	answerDiv.className = "questionWrap";
+	answerEl.type = "radio";
+	answerEl.name = "radio" + [i];
+	answerEl.value = question[i].answers[j];
+
+	formGroup.appendChild(answerDiv);
+
+	};
+
 };
-	
-var createQuestion() {
-	// clears previous HTML
-	questionnaire.innerHTML = "";
-	
-// loop
-for (var i = 0; i < i++) {
-	
-	// elements that will be created when page loads
-	var formGroup = document.createElement("div");
-	var questionEl = document.createElement("h2");
-	
-	// attributes that will be added to elements above
-	formGroup.className = "formGroup";
-	questionEl.id = "questions" + [i];
-	
-	// will create text for question
-	var questionText = document.createTextNode(questions[i].question);
-	
-	// will add question to element
-	questionEl.appendChild(questionText);
-	
-	// will add element to DOM
-	formGroup.appendChild(questionEl);
-	
-	// will add formGroup to questionForm
-	questionForm.appendChild(formGroup);
-	
-	// adding questions to be displayed in DOM
-for ( var r = 0; r <questions[i].answers.length; r++) {
-	var answerDiv = document.createElement("div");
-	var answerEl = document.createElement("input");
-	
-	//node for text question
-var answerText = document.createTextNode(questions[i].answers[r];
 
 	
 	
