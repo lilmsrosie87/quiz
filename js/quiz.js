aeaff7e67e389055e9cd5adfebd4db30817080af
Roslyn Harris <rharris0113@gmail.com>
2018-08-15 22:21:58 -0700

    debugging

--- js/quiz.js
+++ js/quiz.js
@@ -2,37 +2,37 @@
 var questions = [
 	{
 		question: "What district did Katnis Everden represent at the 74th Hunger Games?",
-		answer: [" District 12 ", " District 8 ", " District 2", " The Capital "],
+		answers: [" District 12 ", " District 8 ", " District 2", " The Capital "],
 		correct: "District 12"
 	},
 	{
 		question: "What type of shop did Peeta Mellark's family own?",
-		answer:[" apothecary ", " auto ", " bakery ", " farm "],
+		answers:[" apothecary ", " auto ", " bakery ", " farm "],
 		correct: "bakery"
 	},
 	{
 		question: "In her first interview with Ceaser Flickerman, what does Katniss say is her favorite thing about the Capital?",
-		answer:[" the architecture ", " lamb stew with dried plums ", " everyone is so friendly ", " she doesn't say anything "],
+		answers:[" the architecture ", " lamb stew with dried plums ", " everyone is so friendly ", " she doesn't say anything "],
 		correct: "lamb stew with dried plums"
 	},
 	{
 		question: "Who is the mentor for District 12 in the 74th Hunger Games?",
-		answer:[" Effie Trinket ", " Gayle Thompson ", " President Snow ", " Haymitch Abernathy "],
+		answers:[" Effie Trinket ", " Gayle Thompson ", " President Snow ", " Haymitch Abernathy "],
 		correct: "Haymitch Abernathy"
 	},
 	{
 		question: "What three districts were known as 'the careers'?",
-		answer: [" District 13 ", "District 3 ", " The Capital ", " District 6 "],
+		answers: [" District 13 ", "District 3 ", " The Capital ", " District 6 "],
 		correct: "District 3"
 	},
 	{
 		question: "What did Peeta through towards Katniss on that rainy day she saw the dandilions?",
-		answer: [" bread ", " basketball ", " advice ", " a coin from the Capital "],
+		answers: [" bread ", " basketball ", " advice ", " a coin from the Capital "],
 		correct: "bread"
 	},
 	{
 		question: "Who is the only person Katnis is certain that she loves?",
-		answer: [" Peeta ", " Prim ", " Gayle ", " her mother "],
+		answers: [" Peeta ", " Prim ", " Gayle ", " her mother "],
 		correct: "Prim"
 	}
 	
@@ -81,7 +81,7 @@ function createQuestion() {
 		questionnaire.appendChild(formGroup);
 		
 		// adding questions to be displayed in DOM
-	for (var r = 0; r < questions[i].answers.length; r++) {
+	for (var r = 0; r < questions[i].answers.length; r++) {		
 		var answerDiv = document.createElement("div");
 		var answerEl = document.createElement("input");
 		
