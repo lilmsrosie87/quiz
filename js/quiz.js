5a657f44df18075f72c9ff4236d2e54d91a3d9af
Roslyn Harris <rharris0113@gmail.com>
2018-12-02 21:30:56 -0800

    changed text

--- js/quiz.js
+++ js/quiz.js
@@ -1,9 +1,9 @@
 // **update jQuery
-$(document).ready(function() {
-	$(".start").onclick(function() {
-		$("h1").hide();
-	});
-});
+// $(document).ready(function() {
+	// $(".start").onclick(function() {
+		// $("h1").hide();
+	// });
+// });
 
 // questions for quiz (week 10)
 var questions = [
